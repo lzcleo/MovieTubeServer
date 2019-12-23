5531ef359fdce74aff04af6f0ab4379fa0dd7f9e
DCRUNNN <458891338@qq.com>
2019-12-23 14:40:08 +0800

    优化设置管理员权限的实现

--- src/main/java/cn/edu/nju/movietubeserver/service/impl/UserServiceImpl.java
+++ src/main/java/cn/edu/nju/movietubeserver/service/impl/UserServiceImpl.java
@@ -5,6 +5,10 @@ import cn.edu.nju.movietubeserver.dao.PermissionDao;
 import cn.edu.nju.movietubeserver.dao.UserDao;
 import cn.edu.nju.movietubeserver.dao.po.UserPo;
 import cn.edu.nju.movietubeserver.service.UserService;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -31,34 +35,26 @@ public class UserServiceImpl implements UserService
     @Override
     public UserDto getUserByEmail(String email)
     {
-        UserPo userPo = userDao.getUserByEmail(email);
-        if (userPo == null)
-        {
-            throw new UsernameNotFoundException("email not found");
-        }
-        if (StringUtils.equalsIgnoreCase("ROLE_ADMIN", userPo.getRoleName()))
-        {
-            // 超级管理员拥有所有权限
-            userPo.setPermissionCodeList(permissionDao.getAllAuthorityCode());
-        }
-        return userPo.toDto();
+        // 超级管理员拥有所有权限
+        return Optional.ofNullable(userDao.getUserByEmail(email))
+            .map(userPo -> setAdminPermission(userPo,
+                user -> StringUtils.equalsIgnoreCase("ROLE_ADMIN", user.getRoleName()),
+                user -> user.setPermissionCodeList(permissionDao.getAllAuthorityCode())))
+            .map(UserPo::toDto)
+            .orElseThrow(() -> new UsernameNotFoundException("email not found"));
     }
 
     @Override
     public UserDto getUserByUsername(String username)
         throws UsernameNotFoundException
     {
-        UserPo userPo = userDao.getUserByUsername(username);
-        if (userPo == null)
-        {
-            throw new UsernameNotFoundException("username not found");
-        }
-        if (StringUtils.equalsIgnoreCase("ROLE_ADMIN", userPo.getRoleName()))
-        {
-            // 超级管理员拥有所有权限
-            userPo.setPermissionCodeList(permissionDao.getAllAuthorityCode());
-        }
-        return userPo.toDto();
+        // 超级管理员拥有所有权限
+        return Optional.ofNullable(userDao.getUserByUsername(username))
+            .map(userPo -> setAdminPermission(userPo,
+                user -> StringUtils.equalsIgnoreCase("ROLE_ADMIN", user.getRoleName()),
+                user -> user.setPermissionCodeList(permissionDao.getAllAuthorityCode())))
+            .map(UserPo::toDto)
+            .orElseThrow(() -> new UsernameNotFoundException("username not found"));
     }
 
     /**
@@ -71,5 +67,17 @@ public class UserServiceImpl implements UserService
     public boolean verifyPassword(String rawPassword, String encodedPassword)
     {
         return passwordEncoder.matches(rawPassword, encodedPassword);
+    }
+
+    private UserPo setAdminPermission(UserPo userPo, Predicate<UserPo> predicate, Consumer<UserPo> consumer)
+    {
+        Objects.requireNonNull(userPo, "用户不能为空");
+        Objects.requireNonNull(predicate, "判断用户是否是管理员的条件不能为空");
+        Objects.requireNonNull(consumer, "对管理员赋予权限的操作不能为空");
+        if (predicate.test(userPo))
+        {
+            consumer.accept(userPo);
+        }
+        return userPo;
     }
 }
