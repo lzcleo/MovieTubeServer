d30b26e720736782a2676b461120cc54c396784a
DCRUNNN <458891338@qq.com>
2020-01-02 16:09:34 +0800

    新增评论模块的实现

--- src/main/java/cn/edu/nju/movietubeserver/api/CommentAPI.java
+++ src/main/java/cn/edu/nju/movietubeserver/api/CommentAPI.java
@@ -1,6 +1,10 @@
 package cn.edu.nju.movietubeserver.api;
 
-import cn.edu.nju.movietubeserver.api.dto.CommentDto;
+import cn.edu.nju.movietubeserver.model.dto.CommentDto;
+import cn.edu.nju.movietubeserver.model.dto.PostCommentDto;
+import cn.edu.nju.movietubeserver.model.dto.ReceiveCommentDto;
+import cn.edu.nju.movietubeserver.model.dto.ReplyCommentDto;
+import cn.edu.nju.movietubeserver.model.dto.RootCommentDto;
 import cn.edu.nju.movietubeserver.support.response.RestApiResponse;
 import org.springframework.data.domain.Page;
 
@@ -8,12 +12,26 @@ import org.springframework.data.domain.Page;
  * @author leolu
  * @create 2019-12-25-20:17
  **/
-public interface CommentAPI {
+public interface CommentAPI
+{
 
-    RestApiResponse<Page<CommentDto>> listByMovieId(Integer pageNo, Integer pageSize, Integer movieId);
+    // 获取电影的主评论列表
+    RestApiResponse<Page<RootCommentDto>> listRootCommentByMovieId(Integer pageNo, Integer pageSize, Long movieId);
 
-    RestApiResponse<Page<CommentDto>> listByPage(Integer pageNo, Integer pageSize);
+    // 根据电影及父评论，查找子评论回复
+    RestApiResponse<Page<ReplyCommentDto>> listReplyCommentOfRootComment(Integer pageNo, Integer pageSize, Long movieId,
+        String rootCommentId);
 
-    RestApiResponse<Boolean> insertComment(CommentDto commentDto);
+    // 获取用户发出的评论
+    RestApiResponse<Page<PostCommentDto>> listUserPostComments(Integer pageNo, Integer pageSize, Integer userId);
+
+    // 获取用户收到的评论
+    RestApiResponse<Page<ReceiveCommentDto>> listUserReceiveComments(Integer pageNo, Integer pageSize, Integer userId);
+
+    RestApiResponse<Void> insertComment(CommentDto commentDto);
+
+    RestApiResponse<Void> deleteByCommentId(Long commentId);
+
+    RestApiResponse<Void> deleteByMovieId(Long movieId);
 
 }
--- src/main/java/cn/edu/nju/movietubeserver/controller/CommentController.java
+++ src/main/java/cn/edu/nju/movietubeserver/controller/CommentController.java
@@ -1,15 +1,28 @@
 package cn.edu.nju.movietubeserver.controller;
 
 import cn.edu.nju.movietubeserver.api.CommentAPI;
-import cn.edu.nju.movietubeserver.api.dto.CommentDto;
-import cn.edu.nju.movietubeserver.dao.po.CommentPo;
+import cn.edu.nju.movietubeserver.model.domain.SimpleUser;
+import cn.edu.nju.movietubeserver.model.dto.CommentDto;
+import cn.edu.nju.movietubeserver.model.dto.PostCommentDto;
+import cn.edu.nju.movietubeserver.model.dto.ReceiveCommentDto;
+import cn.edu.nju.movietubeserver.model.dto.ReplyCommentDto;
+import cn.edu.nju.movietubeserver.model.dto.RootCommentDto;
+import cn.edu.nju.movietubeserver.model.po.CommentPo;
 import cn.edu.nju.movietubeserver.service.CommentService;
+import cn.edu.nju.movietubeserver.service.UserService;
 import cn.edu.nju.movietubeserver.support.response.RestApiResponse;
 import cn.edu.nju.movietubeserver.support.response.RestApiResponseUtil;
 import cn.edu.nju.movietubeserver.utils.DateUtil;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author leolu
@@ -17,35 +30,100 @@ import org.springframework.web.bind.annotation.*;
  **/
 @RestController
 @RequestMapping(path = "/api/comment")
-public class CommentController implements CommentAPI {
+public class CommentController implements CommentAPI
+{
 
     @Autowired
     private CommentService commentService;
 
+    @Autowired
+    private UserService userService;
+
     @Override
-    @RequestMapping(path = "/listByMovieId", method = RequestMethod.GET)
-    public RestApiResponse<Page<CommentDto>> listByMovieId(@RequestParam(defaultValue = "0") Integer pageNo,
-                                                           @RequestParam(defaultValue = "30") Integer pageSize,
-                                                           @RequestParam Integer movieId)
+    @RequestMapping(path = "/listRootCommentByMovieId", method = RequestMethod.GET)
+    public RestApiResponse<Page<RootCommentDto>> listRootCommentByMovieId(
+        @RequestParam(defaultValue = "0") Integer pageNo, @RequestParam(defaultValue = "20") Integer pageSize,
+        @RequestParam Long movieId)
     {
-        return RestApiResponseUtil.createSuccessResponse(commentService.listByMovieId(pageNo,pageSize,movieId));
+        Map<Integer, SimpleUser> simpleUserMap = userService.listAllSimpleUsers()
+            .stream()
+            .collect(Collectors.toMap(SimpleUser::getUserId, u -> u, (oldValue, newValue) -> newValue));
+        return RestApiResponseUtil.createSuccessResponse(commentService.listRootCommentByMovieId(pageNo,
+            pageSize,
+            movieId,
+            simpleUserMap));
     }
 
     @Override
-    @RequestMapping(path = "/listByPage", method = RequestMethod.GET)
-    public RestApiResponse<Page<CommentDto>> listByPage(@RequestParam(defaultValue = "0") Integer pageNo,
-                                                        @RequestParam(defaultValue = "30") Integer pageSize)
+    @RequestMapping(path = "/listReplyCommentOfRootComment", method = RequestMethod.GET)
+    public RestApiResponse<Page<ReplyCommentDto>> listReplyCommentOfRootComment(
+        @RequestParam(defaultValue = "0") Integer pageNo, @RequestParam(defaultValue = "20") Integer pageSize,
+        @RequestParam Long movieId, @RequestParam String rootCommentId)
     {
-        return RestApiResponseUtil.createSuccessResponse(commentService.listByPage(pageNo, pageSize));
+        Map<Integer, SimpleUser> simpleUserMap = userService.listAllSimpleUsers()
+            .stream()
+            .collect(Collectors.toMap(SimpleUser::getUserId, u -> u, (oldValue, newValue) -> newValue));
+        return RestApiResponseUtil.createSuccessResponse(commentService.listReplyCommentOfRootComment(pageNo,
+            pageSize,
+            movieId,
+            rootCommentId,
+            simpleUserMap));
+    }
+
+    @Override
+    @RequestMapping(path = "/listUserPostComments", method = RequestMethod.GET)
+    public RestApiResponse<Page<PostCommentDto>> listUserPostComments(@RequestParam(defaultValue = "0") Integer pageNo,
+        @RequestParam(defaultValue = "30") Integer pageSize, @RequestParam Integer userId)
+    {
+        Map<Integer, SimpleUser> simpleUserMap = userService.listAllSimpleUsers()
+            .stream()
+            .collect(Collectors.toMap(SimpleUser::getUserId, u -> u, (oldValue, newValue) -> newValue));
+        return RestApiResponseUtil.createSuccessResponse(commentService.listUserPostComments(pageNo,
+            pageSize,
+            userId,
+            simpleUserMap));
+    }
+
+    @Override
+    @RequestMapping(path = "/listUserReceiveComments", method = RequestMethod.GET)
+    public RestApiResponse<Page<ReceiveCommentDto>> listUserReceiveComments(
+        @RequestParam(defaultValue = "0") Integer pageNo, @RequestParam(defaultValue = "30") Integer pageSize,
+        @RequestParam Integer userId)
+    {
+        Map<Integer, SimpleUser> simpleUserMap = userService.listAllSimpleUsers()
+            .stream()
+            .collect(Collectors.toMap(SimpleUser::getUserId, u -> u, (oldValue, newValue) -> newValue));
+        return RestApiResponseUtil.createSuccessResponse(commentService.listUserReceiveComments(pageNo,
+            pageSize,
+            userId,
+            simpleUserMap));
     }
 
     @Override
     @RequestMapping(path = "/insertComment", method = RequestMethod.POST)
-    public RestApiResponse<Boolean> insertComment(@RequestBody CommentDto commentDto) {
-        commentDto.setTime(DateUtil.getCurrentTime());
-        return RestApiResponseUtil.createSuccessResponse(commentService.insertComment(CommentPo.valueOf(commentDto)));
+    public RestApiResponse<Void> insertComment(@RequestBody CommentDto commentDto)
+    {
+        commentDto.setId(StringUtils.EMPTY);
+        commentDto.setCreateTime(DateUtil.getCurrentTime());
+        commentService.insertComment(CommentPo.valueOf(commentDto));
+        return RestApiResponseUtil.createSuccessResponse();
     }
 
+    @Override
+    @RequestMapping(path = "/deleteByCommentId", method = RequestMethod.GET)
+    public RestApiResponse<Void> deleteByCommentId(@RequestParam Long commentId)
+    {
+        //TODO 删除该评论下所有的评论回复
+        commentService.deleteByCommentId(commentId);
+        return RestApiResponseUtil.createSuccessResponse();
+    }
 
+    @Override
+    @RequestMapping(path = "/deleteByMovieId", method = RequestMethod.GET)
+    public RestApiResponse<Void> deleteByMovieId(@RequestParam Long movieId)
+    {
+        commentService.deleteByMovieId(movieId);
+        return RestApiResponseUtil.createSuccessResponse();
+    }
 
 }
