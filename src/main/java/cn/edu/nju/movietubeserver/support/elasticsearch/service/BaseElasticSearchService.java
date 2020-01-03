9477823af003b6d83800be4e6b0131ad57f57eac
DCRUNNN <458891338@qq.com>
2020-01-03 17:28:06 +0800

    新增ES模块多字段联合搜索的实现

--- src/main/java/cn/edu/nju/movietubeserver/support/elasticsearch/service/BaseElasticSearchService.java
+++ src/main/java/cn/edu/nju/movietubeserver/support/elasticsearch/service/BaseElasticSearchService.java
@@ -28,10 +28,13 @@ public interface BaseElasticSearchService<T, E, U extends Serializable>
     Page<T> listByPage(Integer pageNo, Integer pageSize);
 
     // 进行分词分析，并且根据lucene的评分机制(TF/IDF)来进行评分
-    Page<T> matchSearchByKeyword(String propertyKeyword, String value, Pageable pageable, SortBuilder<?> sortBuilder);
+    Page<T> matchSearchByKeyword(String fieldName, String searchKeyword, Pageable pageable, SortBuilder<?> sortBuilder);
+
+    // 多字段联合搜索
+    Page<T> multiMatchSearchByKeyword(Pageable pageable, SortBuilder<?> sortBuilder, String searchKeyword, String... fieldNames);
 
     // 精确匹配，不进行分词分析，文档中必须包含整个搜索的词汇
-    Page<T> termSearchByKeyword(String propertyKeyword, String value, Pageable pageable, SortBuilder<?> sortBuilder);
+    Page<T> termSearchByKeyword(String fieldName, String searchKeyword, Pageable pageable, SortBuilder<?> sortBuilder);
 
     Page<T> search(QueryBuilder queryBuilder, Pageable pageable, FieldSortBuilder fieldSortBuilder);
 
